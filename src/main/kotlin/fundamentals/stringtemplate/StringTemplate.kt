46c2ac7ff3d5e1f396da04bc14e23efcac113219
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-23 22:52:53 +0100

    String Templates

---
+++ src/main/kotlin/fundamentals/stringtemplate/StringTemplate.kt
@@ -0,0 +1,9 @@
+package fundamentals.stringtemplate
+
+fun main() {
+    val name = "ilkay"
+    val age = 36
+
+    println("Name is $name and age is $age")
+    println("Name is ${name.uppercase()} and age is $age")
+}
