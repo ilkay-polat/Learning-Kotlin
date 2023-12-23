5a730d6d4d61dd5a675f563af102e8caf2a6ea9e
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-23 22:11:44 +0100

    Basic Types

---
+++ src/main/kotlin/fundamentals/type/BasicType.kt
@@ -0,0 +1,23 @@
+package fundamentals.type
+
+fun main() {
+    // Whole Number Types
+    val byteValue: Byte = 1
+    val shortValue: Short = 32000
+    val intValue = 8
+    val longValue: Long = 3_500_000_000
+
+    // Floating Point Number Types
+    val doubleValue = 3.14
+    val floatValue = 3.2f
+
+
+    // Boolean Type
+    val booleanValue = true
+
+    // Character Type
+    val charValue = 'c'
+
+    // String Type
+    val stringValue = "Ilkay"
+}
