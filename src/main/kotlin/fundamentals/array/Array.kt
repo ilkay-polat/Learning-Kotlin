b90d573303568e480c239009ea08cc41d1549dcc
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-23 22:32:09 +0100

    Arrays

---
+++ src/main/kotlin/fundamentals/array/Array.kt
@@ -0,0 +1,9 @@
+package fundamentals.array
+
+fun main() {
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+
+    val names = arrayOf("Ilkay", "Polat")
+
+    println("Name are as follows: ${names[0]} and ${names[1]}")
+}
