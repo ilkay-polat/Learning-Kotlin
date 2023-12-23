667f5014633526526c24e9c17b2094fcc9c6d350
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-23 23:20:22 +0100

    Comments

--- src/main/kotlin/fundamentals/comment/Comment.kt
+++ src/main/kotlin/fundamentals/comment/Comment.kt
@@ -1,5 +1,23 @@
 package fundamentals.comment
 
+/**
+ * This is main function
+ *
+ * @param bla bla bla
+ * @return bla bla bla
+ * @since 23.12.2023
+ * @author Ilkay Polat
+ */
 fun main() {
+    // Single Line Comment
 
+    /*
+       Multi
+       Line
+       Comment (a.k.a. Block Comment)
+     */
+
+    /**
+     * KDoc Comment
+     */
 }
