146ee33b7e20dcb8e2bce07ee68f15bbb166b2ba
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-30 12:16:29 +0100

    Classes

---
+++ src/main/kotlin/classes/classes/Main.kt
@@ -0,0 +1,9 @@
+package classes.classes
+
+fun main(){
+    println("Started ...")
+
+    val earth = Planet("Earth", 12742)
+
+    println("Planet created: Name: ${earth.name}, Diameter: ${earth.diameter}")
+}
---
+++ src/main/kotlin/classes/classes/Planet.kt
@@ -0,0 +1,7 @@
+package classes.classes
+
+class Planet (val name: String, val diameter: Int) {
+    init {
+        println("Instance is created with name: $name")
+    }
+}
