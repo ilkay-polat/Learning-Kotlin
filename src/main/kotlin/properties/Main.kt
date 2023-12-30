e63ed070412a4859b3ddeb19f75d6890d09c73f6
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-30 23:51:10 +0100

    Properties

---
+++ src/main/kotlin/properties/Main.kt
@@ -0,0 +1,11 @@
+package properties
+
+fun main(){
+    println("Started ...")
+
+    val earth = Planet("Earth", 12742)
+    println("Planet created: Name: ${earth.name}, Radius: ${earth.radius}, Population: ${earth.population}")
+
+    earth.population = 7_000_000_000
+    println("Planet created: Name: ${earth.name}, Radius: ${earth.radius}, Population: ${earth.population}")
+}
---
+++ src/main/kotlin/properties/Planet.kt
@@ -0,0 +1,14 @@
+package properties
+
+class Planet(val name: String, diameter: Int) {
+    val radius: Int = diameter / 2
+    var population: Long
+    init {
+        println("Instance is created with name: $name")
+        population = 0
+    }
+
+    constructor(name: String, diameter: Int, gaseous: Boolean) : this(name, diameter)
+    constructor(name: String, diameter: Int, gaseous: Boolean, hasLife: Boolean) : this(name, diameter)
+
+}
