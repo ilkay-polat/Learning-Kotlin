6e076d06597d984c91b2a374db55022e61326cc6
ilkaypolat <ilkay.polat@rabobank.nl>
2023-12-31 12:51:42 +0100

    Methods

---
+++ src/main/kotlin/methods/Main.kt
@@ -0,0 +1,14 @@
+package methods
+
+fun main(){
+    println("Started ...")
+
+    val earth = Planet("Earth", 12742)
+    println("Planet created: Name: ${earth.name}, Radius: ${earth.radius}, Population: ${earth.population}")
+
+    earth.population = 7_000_000_000
+    println("Planet created: Name: ${earth.name}, Radius: ${earth.radius}, Population: ${earth.population}")
+
+    val newPopulation = earth.runPopulationModel(0, 1000, 100)
+    println("New population is $newPopulation")
+}
---
+++ src/main/kotlin/methods/Planet.kt
@@ -0,0 +1,22 @@
+package methods
+
+class Planet(val name: String, diameter: Int) {
+    val radius: Int = diameter / 2
+    var population: Long
+    init {
+        println("Instance is created with name: $name")
+        population = 0
+    }
+
+    constructor(name: String, diameter: Int, gaseous: Boolean) : this(name, diameter)
+    constructor(name: String, diameter: Int, gaseous: Boolean, hasLife: Boolean) : this(name, diameter)
+
+    fun runPopulationModel(startDate: Int, endDate: Int, startPopulation: Int) : Long {
+        initPopulationRun(startDate, endDate, startPopulation)
+        return  population
+    }
+
+    private fun initPopulationRun(startDate: Int, endDate: Int, startPopulation: Int) {
+        println("initPopulationRun called!")
+    }
+}
